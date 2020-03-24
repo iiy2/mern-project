346bdb4222f556a1ae2c6e3dc6b4eada45d6ed33
iiy2 <ihor.yushchuk@gmail.com>
2020-03-24 15:56:26 +0200

    Mongo DB config and connection added

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const connectDB = require('./config/db');
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+//connect database
+connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
 
