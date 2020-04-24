cdc50524eba72ae5b158e4625412217ce1672d57
iiy2 <ihor.yushchuk@gmail.com>
2020-04-24 23:43:55 +0300

    Fixed post delete route, add create experience route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -128,14 +128,58 @@ router.get('/user/:user_id', async (req, res) => {
 // @route DELETE api/profile
 // @desc Delete profile, user & posts
 // @access Private
-router.delete('/', async (req, res) => {
+router.delete('/', auth, async (req, res) => {
     try {
         //@TODO remove users posts
         //remove profile
-        await Profile.findOneAndRemove({user: req.params.user_id});
+        await Profile.findOneAndRemove({user: req.user.id});
         //remove user
-        await User.findOneAndRemove({_id: req.params.user_id});
+        await User.findOneAndRemove({_id: req.user.id});
         return res.json({msg: 'User deleted'});
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+});
+
+// @route PUT api/profile/experience
+// @desc Add profile experience
+// @access Private
+router.put('/experience', [auth, [
+    check('title', 'Title is required').not().isEmpty(),
+    check('company', 'Company is required').not().isEmpty(),
+    check('from', 'From date is required').not().isEmpty(),
+]], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    const {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description
+    } = req.body;
+
+    const newExp = {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description
+    };
+
+    try {
+        const profile = await Profile.findOne({user: req.user.id});
+        profile.experience.unshift(newExp);
+        await profile.save();
+        return res.json(profile);
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server error');
