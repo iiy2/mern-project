ffc0a14f7d70983ccf6e59bb9a6154e9a72e62c2
iiy2 <ihor.yushchuk@gmail.com>
2020-04-25 17:22:33 +0300

    Delete profile experience by id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -186,4 +186,25 @@ router.put('/experience', [auth, [
     }
 });
 
+// @route DELETE api/profile/experience/:exp_id
+// @desc Delete experience from profile
+// @access Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({user: req.user.id});
+
+        //get remove index
+        const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
+
+        profile.experience.splice(removeIndex, 1);
+
+        await profile.save();
+
+        return res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+});
+
 module.exports = router;
