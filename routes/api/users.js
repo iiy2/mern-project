2f89bb247a3ef0054a506b00a90330fdf51c0e6c
iiy2 <ihor.yushchuk@gmail.com>
2021-02-13 17:29:37 +0200

    Fixed typo in a message

--- routes/api/users.js
+++ routes/api/users.js
@@ -12,7 +12,7 @@ const User = require('../../models/User');
 router.post('/', [
     check('name', 'Name is required').not().isEmpty(),
     check('email', 'Please provide a valid email').isEmail(),
-    check('password', 'Please provide a pasword with minimal length of 6').isLength({min: 6})
+    check('password', 'Please provide a password with minimal length of 6').isLength({min: 6})
 ], async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
